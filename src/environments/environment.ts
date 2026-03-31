e471a314a62f20ba83cbf2d59d0e66bc25db30a8
ju4n_0 <juanswc87@gmail.com>
2026-03-31 12:07:38 -0300

    feat: add development environment configuration with local API URL

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8080/api'
+};
+
+// sirve para tener un lugar centralizado donde guardar las configuraciones globales
+// de  la aplicacion, nos sirve para no escribir la url multiples veces en el codigo,
